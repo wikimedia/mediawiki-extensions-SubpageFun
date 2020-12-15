5cd267aef209e91ee542ba9a216ea6ce25ba6313
zoranzoki21 <zorandori4444@gmail.com>
2020-12-15 09:36:40 +0100

    Add phpcs and make it pass
    Change-Id: Ic57ae703cee9d43be3f992f27f06197a5660a372

--- SubpageFun.i18n.magic.php
+++ SubpageFun.i18n.magic.php
@@ -1,6 +1,4 @@
 <?php
-#coding: utf-8
-
 /**
  * Internationalization file for magic words of the 'Subpage Fun' extension.
  *
@@ -11,14 +9,14 @@
  * @author Daniel Werner < danweetz@web.de >
  */
 
-$magicWords = array();
+$magicWords = [];
 
-$magicWords['en'] = array(
-		'subpagetitle'     => array ( 1, 'SUBPAGETITLE' ),
-		'subpages'         => array ( 1, 'SUBPAGES' ),
-		'parentpages'      => array ( 1, 'PARENTPAGES' ),
-		'siblingpages'     => array ( 1, 'SIBLINGPAGES' ),
-		'subpagelevel'     => array ( 1, 'SUBPAGELEVEL' ),
-		'numberofsubpages' => array ( 1, 'NUMBEROFSUBPAGES' ),
-		'toplevelpage'     => array ( 1, 'TOPLEVELPAGE' ),
-);
+$magicWords['en'] = [
+	'subpagetitle' => [ 1, 'SUBPAGETITLE' ],
+	'subpages' => [ 1, 'SUBPAGES' ],
+	'parentpages' => [ 1, 'PARENTPAGES' ],
+	'siblingpages' => [ 1, 'SIBLINGPAGES' ],
+	'subpagelevel' => [ 1, 'SUBPAGELEVEL' ],
+	'numberofsubpages' => [ 1, 'NUMBEROFSUBPAGES' ],
+	'toplevelpage' => [ 1, 'TOPLEVELPAGE' ],
+];
